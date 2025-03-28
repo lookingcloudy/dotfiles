4e014629df1e513ccda02556f5eeb66cfee1133a
brad <brad@schulz.com>
2025-03-28 19:01:22 -0400

    add .vscode

--- .vscode/extensions/mohd-akram.vscode-html-format-0.1.6/out/test/suite/extension.test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const vscode = require("vscode");
-suite("Extension Test Suite", () => {
-    vscode.window.showInformationMessage("Start all tests.");
-});
-//# sourceMappingURL=extension.test.js.map
